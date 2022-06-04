7d5fa478a76deac4cdb21bb3197e2f8c11e6ae58
Alex Lobov <lexlobov@yandex.ru>
2022-06-04 19:34:02 +0700

    Собрал все клиенты в один

--- src/test/java/UserSteps.java
+++ src/test/java/UserSteps.java
@@ -1,22 +1,12 @@
-import api.CreateUserApiClient;
-import api.DeleteUserClient;
-import api.LoginUserClient;
+
+import api.UserClient;
 import api.model.UserModel;
-import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
-import org.apache.http.HttpStatus;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-
 
 public class UserSteps {
 
-    CreateUserApiClient createUserApiClient = new CreateUserApiClient();
-    DeleteUserClient deleteUserClient = new DeleteUserClient();
-    LoginUserClient loginUserClient = new LoginUserClient();
+    UserClient userClient = new UserClient();
 
     private String authToken;
 
@@ -30,20 +20,20 @@ public class UserSteps {
 
     @Step("Создание пользователя")
     public void createNewUser(UserModel user) {
-        ValidatableResponse response = createUserApiClient.createUser(user);
+        ValidatableResponse response = userClient.createUser(user);
         setAuthToken(response.extract().path("accessToken"));
     }
 
     @Step("Авторизация пользователя")
     public void loginUserPositive(String email, String password){
         UserModel user = new UserModel(email, password);
-        ValidatableResponse response = loginUserClient.loginUser(user);
+        ValidatableResponse response = userClient.loginUser(user);
         String accessToken = response.extract().path("accessToken");
         setAuthToken(accessToken);
     }
 
     @Step("Удаление пользователя")
     public void deleteUser(){
-        ValidatableResponse response = deleteUserClient.deleteUser(authToken);
+        ValidatableResponse response = userClient.deleteUser(authToken);
     }
 }
